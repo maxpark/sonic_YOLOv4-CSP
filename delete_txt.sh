bea326487828590f1840c548f0f3e56dd6011dd4
Chanyong <77262389+scyonggg@users.noreply.github.com>
2021-07-27 17:07:46 +0900

    2021.7.27. 17:07

---
+++ delete_txt.sh
@@ -0,0 +1,24 @@
+rm /data/chanyong/coco/train2017/*1.txt
+rm /data/chanyong/coco/train2017/*2.txt
+rm /data/chanyong/coco/train2017/*3.txt
+rm /data/chanyong/coco/train2017/*4.txt
+rm /data/chanyong/coco/train2017/*5.txt
+rm /data/chanyong/coco/train2017/*6.txt
+rm /data/chanyong/coco/train2017/*7.txt
+rm /data/chanyong/coco/train2017/*0.txt
+rm /data/chanyong/coco/train2017/*8.txt
+rm /data/chanyong/coco/train2017/*9.txt
+
+
+rm /data/chanyong/coco/val2017/*1.txt
+rm /data/chanyong/coco/val2017/*2.txt
+rm /data/chanyong/coco/val2017/*3.txt
+rm /data/chanyong/coco/val2017/*4.txt
+rm /data/chanyong/coco/val2017/*5.txt
+rm /data/chanyong/coco/val2017/*6.txt
+rm /data/chanyong/coco/val2017/*7.txt
+rm /data/chanyong/coco/val2017/*0.txt
+rm /data/chanyong/coco/val2017/*8.txt
+rm /data/chanyong/coco/val2017/*9.txt
+
+
---
+++ weights2pt.py
@@ -0,0 +1,5 @@
+from models.models import *
+
+
+
+convert('models/yolov4-csp.cfg', 'weights/yolov4-csp.weights')
